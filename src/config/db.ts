3adde54b300cd1798e575a1f8c8880fff010807f
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-04 23:15:40 +0600

    Customers service

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,21 @@
 import { Pool } from "pg";
 import config from ".";
 
-const pool = new Pool({
+ export const pool = new Pool({
     connectionString: `${config.connection_str}`
 })
 
-cosnt 
+const initDb = async () => {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS customers(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL, 
+        email VARCHAR(150) UNIQUE NULL,
+        password TEXT NOT NULL,
+        phone VARCHAR(15),
+        role VARCHAR(50) NOT NULL
+        )
+        `)
+}
+
+export default initDb;
--- src/modules/customers/customers.service.ts
+++ src/modules/customers/customers.service.ts
@@ -1,3 +1,11 @@
-const createCustomer = async () => {
+import bcrypt from "bcryptjs";
+import { pool } from "../../config/db";
+const createCustomer = async (payload: Record<string, unknown>) => {
+    const {name, role, email, password} = payload;
     
+    const hashedPass = await bcrypt.hash(password as string, 10)
+    const result = await pool.query(`INSERT INTO customers(name, role, email, password) VALUES($1, $2, $3, $4) RETURNING *`, [name, role, email, hashedPass])
+    
+    return result;
+
 }
