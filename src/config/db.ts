4aee6c5a36aee533085f0170293dcec7a0480eca
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-06 21:08:44 +0600

    Bookings pg

--- src/config/db.ts
+++ src/config/db.ts
@@ -23,9 +23,23 @@ const initDB = async () => {
         type VARCHAR(50) NOT NULL,
         registration_number VARCHAR(50) UNIQUE NOT NULL,
         daily_rent_price  NUMERIC(10,2) NOT NULL POSITIVE,
-        availability_status BOOLEAN DEFAULT AVAILABLE"
+        availability_status
         )
-        `)    
+        `)
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS bookings(
+            id SERIAL PRIMARY KEY,
+            customer_id INT REFERENCES users(id) on DELETE CASCADE,
+            vehicle_id INT REFERENCES vehicles(id) on DELETE CASCADE,
+            rent_start_date DATE NOT NULL,
+            rent_start_date DATE NOT NULL,
+            rent_end_date DATE NOT NULL,
+            total_price NUMERIC(10,2) NOT NULL POSITIVE,
+            status VARCHAR(50) 
+          )
+            `)    
+
+
 }
 
 export default initDB;
